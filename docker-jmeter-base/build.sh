57b53ed5dd9f7d1de8b946aa54dbbec6e7b31e73
Kuankuan Yang <yangkk@koal.com>
2022-03-10 10:35:36 +0800

    版本号更新

--- docker-jmeter-base/build.sh
+++ docker-jmeter-base/build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker build -t kuankuan0000/docker-jmeter-base:v0.0.1 -t kuankuan0000/docker-jmeter-base:latest .
+
+VERSION=v0.0.6
+docker build -t kuankuan0000/docker-jmeter-base:${VERSION} -t kuankuan0000/docker-jmeter-base:latest .
